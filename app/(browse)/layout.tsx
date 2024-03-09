e9996ed870bf84effac5a3ef15def9e79ee650f1
Qalam-live <156679645+Qalam-live@users.noreply.github.com>
2024-03-09 20:47:11 +0200

    4th

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,3 +1,4 @@
+import { Banner } from "@/components/banner";
 import { Container } from "./_components/container";
 import { Navbar } from "./_components/navbar";
 
@@ -12,6 +13,9 @@ const BrowseLayout = ({
             <Navbar />
             <div className="flex h-full pt-20">
                 <Container>
+            <Banner
+          label="تنويه: الموقع ما زال قيد التطوير , نرجوا محادثتنا في حال واجهتك اي مشكلة"
+        />
                     {children}
                 </Container>
             </div>
